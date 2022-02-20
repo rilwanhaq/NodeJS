bef0acd82bcd229f2a533ad4b8e0a9d2029fcf10
Rilwan_haq <rilwanhaq@gmail.com>
2022-02-20 11:26:20 +0530

    Assignment-2

---
+++ assignments/assignment_2/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const http = require('http');
+
+fs.writeFileSync('index.html',"<h1>Hello World</h1>",err => {
+    console.log(err);
+});
+
+const server = http.createServer((req,res) => {
+    fs.readFile('index.html', {encoding:"utf-8"}, (err,data) => {
+        res.end(data);
+    });
+});
+server.listen(3000, () => console.log("Server is running on port 3000"));
